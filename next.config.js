bb644175044a2beea48eda8b3d36b4dd7b104a06
Paul - Andrei Nafureanu <paul.nafureanu@gmail.com>
2023-08-21 00:16:32 +0300

    Modify for vercel test.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   // enabled: process.env.ANALYZE === "true",
-  enabled: true,
+  enabled: false,
 });
 
 const threeRegExp = /[\\/]node_modules[\\/]three[\\/](src|examples)[\\/]/;
@@ -33,5 +33,5 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
-// module.exports = nextConfig;
+// module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
